463930cdf550187c0c28b5230922973aa4328ba9
Mohammed Talha <talhaqqq@gmail.com>
2016-08-30 15:02:53 +0530

    after updating seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 employee1 = Employee.create(firstname: 'Mohammed', middlename: '', lastname: 'Talha', email: 'talhaqqq@gmail.com', contact:'+91-9947183255' , designation: 'Developer' , dateofjoin: DateTime.new(2009,9,1,17) ,department: 'WebApps' , location:'Kerala', gender: 'Male', maritialstatus: 'Singe' ,  dob: DateTime.new(2009,9,1,17) , temporaryaddress: 'Aluva' , permenantaddress: 'PTA Kerala' , url: '' ,status: 'Active', role: 'Employee')
 user1 = User.create(email: 'admin@test.com', password: 'password' ,role: 'Admin' )
 
-user2 = User.create(email: 'talhaqqq@gmail.com', password: 'password' ,role: 'Employee' ,  employee_id: employee1)
+user2 = User.create(email: 'talhaqqq@gmail.com', password: 'password' ,role: 'Employee' ,  employee_id: 1)
 Attendance.create(employee: employee1 )
 
  # Employee.create(firstname: 'Hassan', middlename: '', lastname: 'Qamar', email: 'qamar23hassan@gmail.com', contact:'+91-9947183255' , designation: 'Developer' , dateofjoin: DateTime.new(2009,9,1,17) ,department: 'WebApps' , location:'Kerala', gender: 'Male', maritialstatus: 'Singe' ,  dob: DateTime.new(2009,9,1,17) , temporaryaddress: 'Aluva' , permenantaddress: 'PTA Kerala' , url: '' ,status: 'Active' ,role: 'Employee')
