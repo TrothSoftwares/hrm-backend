2a16028bc0d462ad817ab4c29c4560d72dc69b4b
anvrnazr <anvrnazr@gmail.com>
2016-09-27 11:05:43 +0530

    after fixing updated at and attendance history

--- app/serializers/attendance_serializer.rb
+++ app/serializers/attendance_serializer.rb
@@ -1,5 +1,11 @@
 class AttendanceSerializer < ActiveModel::Serializer
-  attributes :id ,:totaldays ,:leavedays ,:presentdays ,:status ,:comments
+  attributes :id ,:totaldays ,:leavedays ,:presentdays ,:status ,:comments  ,:updatedat
 
   belongs_to :employee
+
+
+  def updatedat
+    object.updated_at
+  end
+
 end
