0cd117fa27894a4bc1a60f8e680ab3c36f6b949d
talhaqqq <talhaqqq@gmail.com>
2016-10-26 16:17:50 +0530

    after initial config

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :leaverolls
   resources :employees
 
-   devise_for :users, controllers: { sessions: 'sessions' }
+   devise_for :users,  defaults: { format: :json }, controllers: { sessions: 'sessions' }
   #  match 'users/id:' => 'sessions#destroy', via: :delete
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
